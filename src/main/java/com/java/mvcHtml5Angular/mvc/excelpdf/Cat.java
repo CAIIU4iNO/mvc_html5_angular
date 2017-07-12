1199492f556b1c7051eba71456c8575e6f2f3a8a
CAIIU4iNO <caiiu4ino@gmail.com>
2017-07-13 00:07:57 +0300

    upload file and excel or pdf

---
+++ src/main/java/com/java/mvcHtml5Angular/mvc/excelpdf/Cat.java
@@ -0,0 +1,74 @@
+package com.java.mvcHtml5Angular.mvc.excelpdf;
+
+/**
+ * Created by Admin on 12.07.2017.
+ */
+public class Cat {
+    private String name;
+    private String color;
+    private int weight;
+
+    public Cat(String name, String color, int weight){
+        this.name = name;
+        this.color = color;
+        this.weight = weight;
+    }
+
+    public Cat() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                ", color='" + color + '\'' +
+                ", weight=" + weight +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Cat cat = (Cat) o;
+
+        if (weight != cat.weight) return false;
+        if (name != null ? !name.equals(cat.name) : cat.name != null) return false;
+        return color != null ? color.equals(cat.color) : cat.color == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (color != null ? color.hashCode() : 0);
+        result = 31 * result + weight;
+        return result;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+}
--- src/main/webapp/WEB-INF/view/about/about.jsp
+++ src/main/webapp/WEB-INF/view/about/about.jsp
@@ -12,6 +12,8 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<%@ taglib prefix="page" tagdir="/WEB-INF/tags" %>
+
 
 <head>
     <title>Javastudy.ru MVC_HTML5_Angular</title>
